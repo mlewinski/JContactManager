ccc641a4de84f8fd24ee8975a4e846f065fc6f8e
Marek Lewiński <lewinski.marek@outlook.com>
2017-11-10 18:38:06 +0100

    Implemented Communicator model

---
+++ JContactManager/src/org/jcontactmanager/model/Communicator.java
@@ -0,0 +1,25 @@
+package org.jcontactmanager.model;
+
+/**
+ * Created by mlewinski on 11/10/17.
+ */
+public class Communicator {
+    public String label;
+    public String note;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
