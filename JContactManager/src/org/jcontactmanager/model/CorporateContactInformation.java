65702b427039a736c917211ac2d44532431aa48d
Marek Lewiński <lewinski.marek@outlook.com>
2017-11-19 21:44:57 +0100

    Updated CorporateContactInformation model
    Implemented JavaFX properties in moel

--- JContactManager/src/org/jcontactmanager/model/CorporateContactInformation.java
+++ JContactManager/src/org/jcontactmanager/model/CorporateContactInformation.java
@@ -1,23 +1,33 @@
 package org.jcontactmanager.model;
 
+import javafx.beans.property.StringProperty;
+
 public class CorporateContactInformation extends ContactInformation {
-    public String NIP;
-    public String REGON;
+    private StringProperty NIP;
+    private StringProperty REGON;
 
     public String getNIP() {
+        return NIP.get();
+    }
+
+    public StringProperty getNIPProperty() {
         return NIP;
     }
 
     public void setNIP(String NIP) {
-        this.NIP = NIP;
+        this.NIP.set(NIP);
     }
 
     public String getREGON() {
+        return REGON.get();
+    }
+
+    public StringProperty getREGONProperty() {
         return REGON;
     }
 
     public void setREGON(String REGON) {
-        this.REGON = REGON;
+        this.REGON.set(REGON);
     }
 
 }
