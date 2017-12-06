0edad4fc8a675262c13d3f7b389d9ea3e3d0a7d0
mdembiczak <m.dembiczak@wp.pl>
2017-12-06 13:02:59 +0100

    Updated Email model

--- JContactManager/src/org/jcontactmanager/model/Email.java
+++ JContactManager/src/org/jcontactmanager/model/Email.java
@@ -1,12 +1,29 @@
 package org.jcontactmanager.model;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.StringProperty;
 
 /**
  * Created by mlewinski on 11/10/17.
  */
 public class Email extends Communicator{
-    public StringProperty emailAddress;
+    private IntegerProperty id;
+    private StringProperty emailAddress;
+
+    @Override
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty getIdProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getEmailAddress() { return emailAddress.get(); }
 
     public StringProperty getEmailAddressProperty() {
         return emailAddress;
