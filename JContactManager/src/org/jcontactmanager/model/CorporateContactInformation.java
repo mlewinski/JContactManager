e03c25c03a8c17155abd0d397329c439d2c83a04
mdembiczak <m.dembiczak@wp.pl>
2017-12-06 13:02:48 +0100

    Updated CorporateContactInformation model

--- JContactManager/src/org/jcontactmanager/model/CorporateContactInformation.java
+++ JContactManager/src/org/jcontactmanager/model/CorporateContactInformation.java
@@ -1,10 +1,25 @@
 package org.jcontactmanager.model;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.StringProperty;
 
 public class CorporateContactInformation extends ContactInformation {
+    private IntegerProperty id;
     private StringProperty NIP;
     private StringProperty REGON;
+
+    @Override
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
 
     public String getNIP() {
         return NIP.get();
