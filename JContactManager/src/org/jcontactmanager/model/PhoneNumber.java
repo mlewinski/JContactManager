a6192921c7590a44f15d280da5ced6772b7850fc
mdembiczak <m.dembiczak@wp.pl>
2017-12-06 13:03:39 +0100

    Updated PhoneNumber model

--- JContactManager/src/org/jcontactmanager/model/PhoneNumber.java
+++ JContactManager/src/org/jcontactmanager/model/PhoneNumber.java
@@ -1,13 +1,28 @@
 package org.jcontactmanager.model;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.StringProperty;
 
 /**
  * Created by mlewinski on 11/10/17.
  */
 public class PhoneNumber extends Communicator{
+    private IntegerProperty id;
     private StringProperty number;
     private StringProperty Network;
+
+    @Override
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty getIdProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
 
     public String getNumber() {
         return number.get();
