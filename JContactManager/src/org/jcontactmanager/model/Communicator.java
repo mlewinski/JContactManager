388d808d14b0a2492da0267d53a6e2de1998c9d1
mdembiczak <m.dembiczak@wp.pl>
2017-12-06 13:01:47 +0100

    Updated Communicator model

--- JContactManager/src/org/jcontactmanager/model/Communicator.java
+++ JContactManager/src/org/jcontactmanager/model/Communicator.java
@@ -1,13 +1,40 @@
 package org.jcontactmanager.model;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.StringProperty;
 
 /**
  * Created by mlewinski on 11/10/17.
  */
 public class Communicator {
+    protected IntegerProperty id;
     protected StringProperty label;
     protected StringProperty note;
+    protected StringProperty category;
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty getIdProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getCategory() {
+        return category.get();
+    }
+
+    public StringProperty getTypeProperty() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category.set(category);
+    }
 
     public String getLabel() {
         return label.get();
