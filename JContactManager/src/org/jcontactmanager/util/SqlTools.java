2b751bc2f31865c96541e80b56a743efe4ca523c
Marek Lewiński <lewinski.marek@outlook.com>
2017-12-10 14:13:34 +0100

    Implemented SQL sanitizing method
    Implemented org.jcontactmanager.model.util.SqlTools::sanitizeQuery to escape special characters within SQL queries

---
+++ JContactManager/src/org/jcontactmanager/util/SqlTools.java
@@ -0,0 +1,19 @@
+package org.jcontactmanager.util;
+
+public class SqlTools {
+    /**
+     * Sanitizes SQL query parameters by performing character-escaping
+     * @param query Query to be sanitized
+     * @return Sanitized query
+     */
+    public static String removeSpecialChars(String query){
+        String sanitizedQuery = query
+                        .replace("\"", "\\\"")
+                        .replace("'","\\'")
+                        .replace("%","\\%")
+                        .replace("_", "\\_")
+                        .replace("\b", "\\b")
+                        .replace("\t","\\t");
+        return sanitizedQuery;
+    }
+}
