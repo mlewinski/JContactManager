0975059228a19228a6dc73320979d84ee28c0eca
Marek Lewiński <lewinski.marek@outlook.com>
2017-12-12 12:11:59 +0100

    Updated model.ContactRepository
    Added getConnection method

--- JContactManager/src/org/jcontactmanager/model/ContactRepository.java
+++ JContactManager/src/org/jcontactmanager/model/ContactRepository.java
@@ -1,4 +1,23 @@
 package org.jcontactmanager.model;
 
+import java.nio.file.*;
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+
+
 public class ContactRepository {
+
+    private static Connection getConnection() throws SQLException, IOException{
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get(".","resources", "database.properties").normalize())){
+            props.load(in);
+        }
+        String drivers = props.getProperty("jdbc.drivers");
+        if(drivers!=null) System.setProperty("jdbc.drivers", drivers);
+        String url = props.getProperty("jdbc.url");
+        String uname = props.getProperty("jdbc.username");
+        String pass = props.getProperty("jdbc.password");
+        return DriverManager.getConnection(url, uname, pass);
+    }
 }
