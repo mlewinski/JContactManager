a310873ea6f8052f535862bebe21ee2d0b9a4225
mdembiczak <m.dembiczak@wp.pl>
2017-12-24 14:35:13 +0100

    Updated JavaFxMain

--- JContactManager/src/org/jcontactmanager/JavaFxMain.java
+++ JContactManager/src/org/jcontactmanager/JavaFxMain.java
@@ -1,12 +1,15 @@
 package org.jcontactmanager;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.jcontactmanager.model.ContactInformation;
 
 import java.io.IOException;
 
@@ -19,6 +22,8 @@ public class JavaFxMain extends Application {
     private Stage primaryStage;
     private BorderPane rootLayout;
 
+    private ObservableList<ContactInformation> contactInformationData = FXCollections.observableArrayList();
+
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -30,14 +35,26 @@ public class JavaFxMain extends Application {
 
     }
 
+    public JavaFxMain(){
+        contactInformationData.add(new ContactInformation(1,"name","nickname","gender","addres","city","contry","note","website"));
+    }
+
+    public ObservableList<ContactInformation> getContactInformationData() {
+        return contactInformationData;
+    }
+
     public void initRootLayout() {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(JavaFxMain.class.getResource("view/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
 
+            //Show the scene containing root layout
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
+
+            //TODO: Controller to RootLayout
+
             primaryStage.show();
         }catch (IOException e)
         {
@@ -48,7 +65,7 @@ public class JavaFxMain extends Application {
     public void showPersonOverview(){
         try{
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(JavaFxMain.class.getResource("view/PersonOverview.fxml"));
+            loader.setLocation(JavaFxMain.class.getResource("view/ContactOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
 
             rootLayout.setCenter(personOverview);
