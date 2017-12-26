626c3116dbae83a0e68db248bac05a4f4f71129e
Marek Lewiński <lewinski.marek@outlook.com>
2017-12-26 10:26:39 +0100

    Implemented application background settings
    Added functionality that allows user to change application appearance

--- JContactManager/src/org/jcontactmanager/JavaFxMain.java
+++ JContactManager/src/org/jcontactmanager/JavaFxMain.java
@@ -6,20 +6,27 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import org.jcontactmanager.model.Contact;
 import org.jcontactmanager.model.ContactInformation;
 import org.jcontactmanager.model.ContactRepository;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 public class JavaFxMain extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    private Properties applicationProperties;
 
     private Stage primaryStage;
     private BorderPane rootLayout;
@@ -43,6 +50,17 @@ public class JavaFxMain extends Application {
         //contactInformationData.add(new ContactInformation(1,"name","nickname","gender","addres","city","contry","note","website"));
         repository = new ContactRepository();
         contactInformationData = repository.getRepositoryReference();
+        try {
+            applicationProperties = new Properties();
+            applicationProperties.load(new FileInputStream("resources/app.properties"));
+        }
+        catch(IOException ex){
+            applicationProperties = null;
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("Warning - properties");
+            alert.setContentText("Application has encountered a problem while trying to access resources/app.properties file. Application will run using the default config.");
+            alert.showAndWait();
+        }
     }
 
     public ObservableList<Contact> getContactInformationData() {
@@ -55,10 +73,18 @@ public class JavaFxMain extends Application {
             loader.setLocation(JavaFxMain.class.getResource("view/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
 
+            if(applicationProperties.containsKey("view.background")) {
+                File f = new File("resources/" + applicationProperties.getProperty("view.background"));
+                if (f.exists()) {
+                    BackgroundImage backgroundImage = new BackgroundImage(new Image(applicationProperties.getProperty("view.background"), primaryStage.getWidth(), primaryStage.getHeight(), false, true),
+                            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+                    rootLayout.setBackground(new Background(backgroundImage));
+                }
+            }
+
             //Show the scene containing root layout
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
-
             //TODO: Controller to RootLayout
 
             primaryStage.show();
