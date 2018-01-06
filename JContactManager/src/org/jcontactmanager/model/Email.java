2a3c6932e499af614330a7e06c70d1a4465e858f
mdembiczak <m.dembiczak@wp.pl>
2018-01-06 19:02:11 +0100

    Updated Email model

--- JContactManager/src/org/jcontactmanager/model/Email.java
+++ JContactManager/src/org/jcontactmanager/model/Email.java
@@ -17,7 +17,22 @@ import static org.jcontactmanager.util.SqlTools.sanitizeQuery;
  */
 public class Email extends Communicator{
     private IntegerProperty id = new SimpleIntegerProperty();
-    private StringProperty emailAddress = new SimpleStringProperty();
+    private StringProperty privateEmailAddress = new SimpleStringProperty();
+    private StringProperty workEmailAddress = new SimpleStringProperty();
+
+    public Email() {
+        this.id = null;
+        this.privateEmailAddress = null;
+        this.workEmailAddress = null;
+        this.note = null;
+    }
+
+    public Email(int id, String privateEmailAddress, String workEmailAddress, String note) {
+        this.id = new SimpleIntegerProperty(id);
+        this.privateEmailAddress = new SimpleStringProperty(privateEmailAddress);
+        this.workEmailAddress = new SimpleStringProperty(workEmailAddress);
+        this.note = new SimpleStringProperty(note);
+    }
 
     @Override
     public int getId() {
@@ -32,31 +47,59 @@ public class Email extends Communicator{
         this.id.set(id);
     }
 
-    public String getEmailAddress() {
-        return emailAddress.get();
+    @Override
+    public String getNote() {
+        return super.getNote();
     }
 
-    public StringProperty emailAddressProperty() {
-        return emailAddress;
+    @Override
+    public StringProperty getNoteProperty() {
+        return super.getNoteProperty();
     }
 
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress.set(emailAddress);
+    @Override
+    public void setNote(String note) {
+        super.setNote(note);
+    }
+
+
+    public String getPrivateEmailAddress() {
+        return privateEmailAddress.get();
+    }
+
+    public StringProperty privateEmailAddressProperty() {
+        return privateEmailAddress;
+    }
+
+    public void setPrivateEmailAddress(String privateEmailAddress) {
+        this.privateEmailAddress.set(privateEmailAddress);
+    }
+
+    public String getWorkEmailAddress() {
+        return workEmailAddress.get();
+    }
+
+    public StringProperty workEmailAddressProperty() {
+        return workEmailAddress;
+    }
+
+    public void setWorkEmailAddress(String workEmailAddress) {
+        this.workEmailAddress.set(workEmailAddress);
     }
 
     @Override
     public String saveQuery(){
-        return sanitizeQuery("INSERT INTO Emails VALUES(" + this.id + "," + this.emailAddress+")");
+        return sanitizeQuery("INSERT INTO Emails VALUES(" + this.id + "," + this.privateEmailAddress + "," + this.workEmailAddress + ")");
     }
 
     @Override
     public String updateQuery() {
-        return sanitizeQuery("UPDATE Emails SET ID="+this.id+", EmailAddress="+this.emailAddress);
+        return sanitizeQuery("UPDATE Emails SET ID="+this.id+", PrivateEmailAddress=" + this.privateEmailAddress + ", WorkEmailAddress=" + this.workEmailAddress);
     }
 
     @Override
     public String deleteQuery() {
-        return sanitizeQuery("DELETE FROM Emails WHERE ID =" + this.id + " AND EmailAddress=" + this.emailAddress);
+        return sanitizeQuery("DELETE FROM Emails WHERE ID =" + this.id + " AND PrivateEmailAddress=" + this.privateEmailAddress+ " AND WorkEmailAddress="+ this.workEmailAddress);
     }
 
     @Override
