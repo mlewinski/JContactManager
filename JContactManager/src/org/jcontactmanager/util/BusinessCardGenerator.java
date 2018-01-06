ebc032f0d2e8564f185674455fd155a375db4243
Marek Lewiński <lewinski.marek@outlook.com>
2018-01-06 15:55:49 +0100

    Implemented BusinessCardGenerator

--- JContactManager/src/org/jcontactmanager/util/BusinessCardGenerator.java
+++ JContactManager/src/org/jcontactmanager/util/BusinessCardGenerator.java
@@ -1,8 +1,31 @@
 package org.jcontactmanager.util;
 
-import com.itextpdf.io.*;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import org.jcontactmanager.model.Contact;
+import org.jcontactmanager.model.ContactInformation;
+import org.slf4j.*;
+
+import java.io.IOException;
 
 
 public class BusinessCardGenerator {
-
+    public void generateBusinessCard(Contact contact, String path){
+        try {
+            ContactInformation ci = contact.getContactInformation();
+            PdfWriter pdfWriter = new PdfWriter(path);
+            PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+            Document doc = new Document(pdfDocument);
+            doc.add(new Paragraph("\t\t\t" + ci.getName()));
+            doc.add(new Paragraph("\t" + ci.getAddress()+", "+ci.getCity()+" "+ci.getCountry()));
+            doc.add(new Paragraph("\t" + contact.getWorkEmailAddress().getEmailAddress()));
+            doc.add(new Paragraph("\t" + contact.getWorkPhoneNumber().getNumber()));
+            doc.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
